77132c37742b6fbc446c679f532ab1c74d1aa0cf
Aleksandr Fedorov <fairk19@gmail.com>
2018-05-06 21:30:45 +0300

    display list pokemons and load from server

---
+++ PokedexClient/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  PokedexClient
+//
+//  Created by Александр Федоров on 06.05.2018.
+//  Copyright © 2018 Александр Федоров. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+}
